9ea3b8dbfceb9485a38ac67850e8ebd55368da35
tdavis0828 <davis.t828@gmail.com>
2021-11-03 19:02:51 -0700

    Daily Challenge

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById("button");
+
+btn.addEventListener("click", function () {
+  alert("test");
+});
+
+const word = "Supercalifragilisticexpialidocious";
+const number = BigInt(14 * 311351321);
+
+console.log(word.length);
+console.log(12);
+console.log(typeof 12);
+console.log(number);
+console.log(typeof number);
